6e99996436bd66b95dcdcf7cf46a8333ae37c5c9
sahaantara567 <sahaantara567@gmail.com>
2026-07-15 12:14:55 +0600

    added new changes in restaurant details section

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,17 @@ import userRoutes from "./routes/user.routes.js";
 import restaurantRoutes from "./routes/restaurant.routes.js";
 import connectDB from "./config/db.js";
 
-dotenv.config();
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.join(__dirname, ".env"),
+});
+
 console.log("🔥 MONGO_URI =", process.env.MONGO_URI);
 
 const app = express();
